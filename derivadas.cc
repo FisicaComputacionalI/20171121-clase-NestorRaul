3b9ac8ba0c2a22a5800154a7716f5ded3f980f95
NestorRaul <31254585+NestorRaul@users.noreply.github.com>
2017-11-21 14:01:05 -0600

    Add files via upload

---
+++ derivadas.cc
@@ -0,0 +1,14 @@
+//derivada del coseno por aproximaciones?
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+ float p;
+ float s=cos(p);
+ cout<<"Da un valor para evaluar"<<endl;
+ cin>>p;
+ cout<<"El valor evaluado en el coseno es"<<s<<endl;
+ return 0;
+}
+ 
